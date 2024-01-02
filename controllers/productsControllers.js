26a9a670b77ce715ba69dca8c43df0fc7bd8f6cd
mrzor <zor.muhammet01@gmail.com>
2024-01-02 21:31:18 +0300

    testingg

---
+++ controllers/productsControllers.js
@@ -0,0 +1,53 @@
+const Product = require('../models/Products');
+
+module.exports = {
+    createProduct: async (req, res) => {
+        const newProduct = new Product(req.body);
+        try {
+            await newProduct.save();
+            res.status(200).json("Product Created Succesfully")
+        } catch (error) {
+            res.status(500).json("Failed to Create Product")
+        }
+    },
+    getAllProduct: async (req, res) => {
+        try {
+            const products = await Product.find().sort({ createdAt: -1 })
+            res.status(200).json(products)
+        } catch (error) {
+            res.status(500).json("Failed to get Products all")
+
+        }
+    },
+    getProduct: async (req, res) => {
+        try {
+            const product = await Product.findById(req.params.id)
+            res.status(200).json(product)
+        } catch (error) {
+            res.status(500).json("Failed to get Product by id")
+        }
+    },
+    searchProduct: async (req, res) => {
+        try {
+            const result = Product.aggregate(
+                [
+                    {
+                        $search: {
+                            index: "MyShopApp",
+                            text: {
+                                query: req.params.key,
+                                path: {
+                                    wildcard: "*"
+                                }
+                            }
+                        }
+                    }
+                ]
+            )
+
+            res.status(200).json(result)
+        } catch (error) {
+            res.status(500).json("Failed to get Products search")
+        }
+    }
+}
