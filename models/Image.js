1d021b61e1c140a86746ba760e2b37408a1c7f8d
mrzor <zor.muhammet01@gmail.com>
2024-01-09 12:45:20 +0300

    image test

--- models/Image.js
+++ models/Image.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const ImageSchema = new mongoose.Schema({
-    // isRemoved: { type: Boolean, default:0},
-    // productId:{type:String, required:true },
-    image:{type:String},
+    // isRemoved: { type: Boolean, default: 0 },
+    // productId: { type: String, required: true },
+    image: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Image", ImageSchema);
